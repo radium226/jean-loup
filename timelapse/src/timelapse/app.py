a92e09ee7dceb1b2378ff479d78fa933664f4a4a
radium226 <adrien.besnard@gmail.com>
2024-01-06 11:26:58 +0100

    WIP

--- timelapse/src/timelapse/app.py
+++ timelapse/src/timelapse/app.py
@@ -13,19 +13,32 @@ def app(context: Context):
 
 
 @app.command()
-@argument("file_path", type=Path)
+@option("--file", "file_path", type=Path, required=False, default=None, help="File path")
 @pass_context
-def take_picture(context: Context, file_path: Path):
+def take_picture(context: Context, file_path: Path | None):
     with System() as system:
+        if not file_path:
+            now = system.now()
+            file_path = Path("/var/lib/timelapse") / "{date_time}.jpg".format(date_time=now.to_iso8601_string())
+        
         system.take_picture(file_path)
 
 
 @app.command()
-@option("--threshold", "threshold_in_seconds", type=int, help="Threshold in seconds", default=60)
+@option("--threshold", "threshold_in_seconds", type=int, help="Threshold (in seconds)", default=60)
+@option("--force", "force", is_flag=True, show_default=True, default=False, help="Force shutdown")
+@option("--dry-run", "dry_run", is_flag=True, show_default=True, default=False, help="Dry run")
+@option("--delay", "delay_in_minutes", default=2, help="Delay (in minutes)")
 @pass_context
-def manage(context: Context, threshold_in_seconds: int):
+def manage(context: Context, threshold_in_seconds: int, delay_in_minutes: int, force: bool, dry_run: bool):
     with System() as system:
-        if system.now().diff(system.wake_up_at).in_seconds() <= threshold_in_seconds:
-            system.take_picture()
-            system.schedule_wakeup(at=system.now().add(minutes=5))
-            system.shutdown()
+        now = system.now()
+        if force or now.diff(system.wake_up_at).in_seconds() <= threshold_in_seconds:
+            file_path = Path("/var/lib/timelapse") / "{date_time}.jpg".format(date_time=now.to_iso8601_string())
+            system.take_picture(file_path)
+            system.schedule_wakeup(at=system.now().add(minutes=delay_in_minutes))
+            if not dry_run:
+                print("Actually shuting down...")
+                system.shutdown()
+            else:
+                print("We should have shut down...")
--- timelapse/src/timelapse/system.py
+++ timelapse/src/timelapse/system.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import os
 import json
 from subprocess import run
+from time import sleep
 
 
 class PictureFormat(StrEnum):
@@ -27,8 +28,9 @@ class System():
 
 
     def __enter__(self):
+        sleep(15)
         pisugar_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        pisugar_socket = pisugar_socket.connect(str(self.pisugar_socket_path))
+        pisugar_socket.connect(str(self.pisugar_socket_path))
         self.pisugar_socket = pisugar_socket
         return self
     
@@ -39,24 +41,38 @@ class System():
 
     @property
     def wake_up_at(self) -> DateTime:
-        return pendulum.parse(json.loads(str(self.pisugar_config_path))["auto_wake_time"])
+        with open(str(self.pisugar_config_path)) as f:
+            return pendulum.parse(json.load(f)["auto_wake_time"])
+    
+    def now(self) -> DateTime:
+        if ( pisugar_socket := self.pisugar_socket ):
+            input = f"get rtc_time"
+            print(f"input={input}")
+            pisugar_socket.sendall(input.encode('utf-8'))
+            output = pisugar_socket.recv(1024)
+            print(f"output={output}")
+            return pendulum.parse(output.decode('utf-8').replace("rtc_time: ", ""))
 
     def schedule_wakeup(self, at: DateTime) -> None:
         if ( pisugar_socket := self.pisugar_socket ):
-            input = f"rtc_alarm_set {at.to_iso8601_string()} 127"
+            input = f"rtc_alarm_set {at} 127"
+            print(f"input={input}")
             pisugar_socket.sendall(input.encode('utf-8'))
             output = pisugar_socket.recv(1024)
-            print(output)
+            print(f"output={output}")
         
     def shutdown(self) -> None:
-        os.system("shutdown")
+        command = ["systemctl", "--no-block", "poweroff", "--check-inhibitors=no"]
+        process = run(command)
+        if process.returncode != 0:
+            raise Exception("Unable to shutdown! ")
 
     def take_picture(self, file_path: Path) -> None:
         command = [
             "rpicam-still",
+            "--nopreview",
             "--immediate",
             "--autofocus-on-capture",
-            "--encoder", "jpg",
             "--output", str(file_path)
         ]
         process = run(command)
