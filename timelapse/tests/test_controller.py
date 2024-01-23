4d3d491fff871a4bec2d63dcdfe2ecde6fcaa741
radium226 <adrien.besnard@gmail.com>
2024-01-23 22:54:56 +0100

    WIP

--- timelapse/tests/test_controller.py
+++ timelapse/tests/test_controller.py
@@ -14,40 +14,6 @@ from .mocks import (
     StorageMock,
 )
 
-@fixture
-def config() -> Config:
-    return Config.default()
-
-
-@fixture
-def pi_sugar() -> PiSugarMock:
-    return PiSugarMock()
-
-
-@fixture
-def system() -> SystemMock:
-    return SystemMock()
-
-
-@fixture
-def camera() -> CameraMock:
-    return CameraMock()
-
-@fixture
-def storage() -> StorageMock:
-    return StorageMock()
-
-
-@fixture
-def controller(config: Config, pi_sugar: PiSugarMock, system: SystemMock, camera: CameraMock, storage: StorageMock) -> Controller:
-    return Controller(
-        config,
-        pi_sugar=pi_sugar, 
-        system=system, 
-        camera=camera,
-        storage=storage,
-    )
-
 
 @mark.parametrize(
     "wakeup_time, number_of_services, number_of_pictures_taken, number_of_scheduled_services",
